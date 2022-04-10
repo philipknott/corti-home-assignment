b15b8ed3c4ce107dd3d8484419900b622081cfce
Philip <philip@cyberline.com>
2022-04-10 09:28:44 +0200

    Folder navigation in main window

--- src/components/GridNavigationItem.tsx
+++ src/components/GridNavigationItem.tsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react';
 import InsertDriveFile from '@mui/icons-material/InsertDriveFileOutlined';
 import Image from '@mui/icons-material/ImageOutlined';
 import FolderIcon from '@mui/icons-material/Folder';
+import Button from "@mui/material/Button"
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 
 import { FlatDataType } from '../../types/types';
+import { AppContext } from '../AppContext';
 
 export const GridNavigationItem = ({ item }: { item: FlatDataType }) => {
+    const appContext = useContext(AppContext);
     let icon = null;
+
     switch (item.type) {
         case 'doc':
             icon = <InsertDriveFile fontSize="large" />;
@@ -21,11 +26,12 @@ export const GridNavigationItem = ({ item }: { item: FlatDataType }) => {
     }
 
     return (
-        <Grid item xs={2}>
-            <Box sx={{ p: 1, border: '1px solid grey' }}>
+        <Grid item xs={2} onClick={() => appContext.setSelectedFile(item)}>
+            <Box sx={{p: 1, border: '1px solid grey', ":hover": { boxShadow: 6 } }}>
                 {icon}
                 <p>{item.name}</p>
             </Box>
         </Grid>
+
     )
 }
